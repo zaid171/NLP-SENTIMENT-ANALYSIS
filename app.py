e0a390eab163beb12e07216f20c7ed715d84c260
Mohammed Zaid p <82380534+zaid171@users.noreply.github.com>
2025-09-24 12:38:05 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import joblib
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Load model + vectorizer
+model = joblib.load("/content/sentiment_model.pkl")
+vectorizer = joblib.load("/content/vectorizer.pkl")
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"[^a-z\s]", "", text)
+    return text
+
+st.title("📊 AI Echo - Sentiment Analysis")
+user_input = st.text_area("Enter a review:")
+
+if st.button("Analyze"):
+    if user_input.strip():
+        cleaned = clean_text(user_input)
+        vec = vectorizer.transform([cleaned])
+        prediction = model.predict(vec)[0]
+        st.success(f"Predicted Sentiment: {prediction}")
